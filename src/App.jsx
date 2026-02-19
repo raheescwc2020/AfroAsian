9cca7821f9d8d548710b5633f3aa56a91c0e1521
Rahees Mohammed <rahees.cwc2020@gmail.com>
2026-02-19 08:18:56 +0530

    Gold Series Content Uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import ArticleGold from './pages/Article_Gold';
+import './ArticleTypography.css';
 import Footer from './components/Footer';
 // Placeholder for other pages to prevent errors
 const Placeholder = ({ title }) => (
@@ -17,7 +19,7 @@ const App = () => {
       <div className="aan-app">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ArticleGold  />} />
           <Route path="/policy" element={<Placeholder title="Policy" />} />
           <Route path="/diplomacy" element={<Placeholder title="Diplomacy" />} />
           <Route path="/economy" element={<Placeholder title="Economy" />} />
